7bead78ca9eef75e357e6e9bef06eaf82512dcda
reza ramezani <rezaaa555555r@gmail.com>
2021-12-27 15:46:53 +0330

    fix eslint errors

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,7 @@
-window.matchMedia =
-	window.matchMedia ||
-	function() {
-		return {
-			matches: false,
-			addListener: function() {},
-			removeListener: function() {}
-		};
-	};
+window.matchMedia = window.matchMedia || function () {
+  return {
+    matches: false,
+    addListener () {},
+    removeListener () {}
+  }
+}
