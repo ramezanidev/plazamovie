0c564e1849ecc2e04402ce5c672cad2e2aa5f058
reza ramezani <rezaaa555555r@gmail.com>
2022-01-18 17:30:31 +0330

    responsive subscription page (mobile)

--- pages/subscription/bronze.vue
+++ pages/subscription/bronze.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="container mx-auto">
-    <div class="w-full pt-12 grid grid-cols-10 gap-4">
+    <div class="w-full pt-12 flex flex-col-reverse md:grid grid-cols-10 gap-4">
       <div class="relative overflow h-80 rounded col-start-1 col-end-5">
         <div class="bg-gradient-to-b from-brand-4 absolute inset-0" />
         <div class="relative inset-0 h-full w-full p-4">
-          <h4 class="text-white font-bold w-full text-center text-lg border-b border-brand-1 border-opacity-75 pb-2">
+          <h4
+            class="
+              text-white
+              font-bold
+              w-full
+              text-center text-lg
+              border-b border-brand-1 border-opacity-75
+              pb-2
+            "
+          >
             خرید اشتراک برنزی
           </h4>
           <ul class="mt-4 text-gray-100 text-sm px-2 space-y-2">
@@ -12,23 +21,53 @@
             <li>مدت زمان استفاده:‌ 35 روز</li>
             <li>تعداد خریداری شده توسط دیگر کاربران:‌ 140</li>
           </ul>
-          <button class="absolute bottom-0 left-0 right-0 w-full h-12 rounded bg-brand-1 hover:bg-brand-4 transition-all text-white font-bold">
+          <button
+            class="
+              absolute
+              bottom-0
+              left-0
+              right-0
+              w-full
+              h-12
+              rounded
+              bg-brand-1
+              hover:bg-brand-4
+              transition-all
+              text-white
+              font-bold
+            "
+          >
             خرید
           </button>
         </div>
       </div>
       <!--  -->
       <div class="relative overflow rounded col-start-5 col-end-11 pb-4">
-        <div class="bg-gradient-to-b from-brand-4 to-brand-2 absolute inset-0" />
+        <div
+          class="bg-gradient-to-b from-brand-4 to-brand-2 absolute inset-0"
+        />
         <div class="relative inset-0 h-full w-full p-4">
-          <h4 class="text-white font-bold w-full text-lg border-b border-brand-1 border-opacity-75 pb-2">
+          <h4
+            class="
+              text-white
+              font-bold
+              w-full
+              text-lg
+              border-b border-brand-1 border-opacity-75
+              pb-2
+            "
+          >
             اشتراک برنزی
           </h4>
-          <h4 class="text-white font-bold w-full text-lg py-4 ">
+          <h4 class="text-white font-bold w-full text-lg py-4">
             اشتراک برنزی چیست؟
           </h4>
           <p class="text-gray-200 text-sm pt-2">
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در ص د.
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+            استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
+            ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز،
+            و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای
+            زیادی در ص د.
           </p>
           <h4 class="text-white font-bold w-full text-lg pt-4 pb-2">
             چرا اشتراک برنزی را تهیه کنیم؟
@@ -46,31 +85,65 @@
     <div class="mt-20">
       <div class="w-full h-1 bg-opacity-60 bg-brand-1 relative mb-20">
         <span
-          class="bg-brand-3 text-white font-bold right-0 top-1/2 absolute transform -translate-y-1/2 pl-4 text-lg select-none"
-        >اشتراک های دیگر </span>
+          class="
+            bg-brand-3
+            text-white
+            font-bold
+            right-0
+            top-1/2
+            absolute
+            transform
+            -translate-y-1/2
+            pl-4
+            text-lg
+            select-none
+          "
+        >اشتراک های دیگر
+        </span>
       </div>
-      <div class="flex justify-around">
+      <div class="flex space-y-4 md:space-y-0 flex-col md:flex-row justify-around">
         <!-- gold -->
-        <div class="relative overflow h-80 w-72 rounded">
-          <div class="bg-gradient-to-b from-brand-4 absolute inset-0 opacity-40" />
+        <div class="relative overflow h-80 w-full md:w-72 rounded">
+          <div
+            class="bg-gradient-to-b from-brand-4 absolute inset-0 opacity-40"
+          />
           <div class="relative inset-0 h-full w-full p-4">
             <h4 class="text-yellow-500 font-bold w-full text-center text-lg">
               اشتراک طلایی
             </h4>
             <ul class="check mt-4 text-gray-300 text-sm px-2 space-y-2">
-              <li>
-                دانلود 5 تا فیلم سینمایی
-              </li>
+              <li>دانلود 5 تا فیلم سینمایی</li>
               <li>15 روز حق استفاده از اشتراک</li>
             </ul>
-            <nuxt-link to="/subscription/golden" class="absolute flex justify-center items-center bottom-0 left-0 right-0 w-full h-12 rounded bg-brand-1 hover:bg-brand-4 transition-all text-white font-bold">
+            <nuxt-link
+              to="/subscription/golden"
+              class="
+                absolute
+                flex
+                justify-center
+                items-center
+                bottom-0
+                left-0
+                right-0
+                w-full
+                h-12
+                rounded
+                bg-brand-1
+                hover:bg-brand-4
+                transition-all
+                text-white
+                font-bold
+              "
+            >
               خرید + جزئیات بیشتر
             </nuxt-link>
           </div>
         </div>
         <!-- silver -->
-        <div class="relative overflow w-72 h-80 rounded">
-          <div class="bg-gradient-to-b from-brand-4 absolute inset-0 opacity-40" />
+        <div class="relative overflow w-full md:w-72 h-80 rounded">
+          <div
+            class="bg-gradient-to-b from-brand-4 absolute inset-0 opacity-40"
+          />
           <div class="relative inset-0 h-full w-full p-4">
             <h4 class="text-white font-bold w-full text-center text-lg">
               اشتراک نقره
@@ -79,7 +152,26 @@
               <li>دانلود 5 تا فیلم سینمایی</li>
               <li>15 روز حق استفاده از اشتراک</li>
             </ul>
-            <nuxt-link to="/subscription/silver" class="absolute flex justify-center items-center bottom-0 left-0 right-0 w-full h-12 rounded bg-brand-1 hover:bg-brand-4 transition-all text-white font-bold">
+            <nuxt-link
+              to="/subscription/silver"
+              class="
+                absolute
+                flex
+                justify-center
+                items-center
+                bottom-0
+                left-0
+                right-0
+                w-full
+                h-12
+                rounded
+                bg-brand-1
+                hover:bg-brand-4
+                transition-all
+                text-white
+                font-bold
+              "
+            >
               خرید + جزئیات بیشتر
             </nuxt-link>
           </div>
@@ -94,15 +186,13 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'SubscriptionBronze',
-  components: {
-
-  }
+  components: {}
 })
 </script>
 
 <style lang="scss" scoped>
 ul.check {
-  list-style-image: url('/check.svg');
+  list-style-image: url("/check.svg");
   @apply px-5;
   ::marker {
     font-size: 35px;
